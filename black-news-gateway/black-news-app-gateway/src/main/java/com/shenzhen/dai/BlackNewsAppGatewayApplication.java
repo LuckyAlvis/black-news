9f2199bd6b924c891b5d119141517471331383ec
ivan <252127196@qq.com>
2024-10-09 11:00:53 +0800

    增加app网关

---
+++ black-news-gateway/black-news-app-gateway/src/main/java/com/shenzhen/dai/BlackNewsAppGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.shenzhen.dai;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BlackNewsAppGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlackNewsAppGatewayApplication.class, args);
+    }
+}
