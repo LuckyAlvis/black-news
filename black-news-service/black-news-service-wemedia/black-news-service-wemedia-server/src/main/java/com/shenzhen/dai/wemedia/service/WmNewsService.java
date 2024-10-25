5e5692b3a08bac91189597938b9d840e0276ca16
ivan <252127196@qq.com>
2024-10-25 13:30:52 +0800

    文章上下架接口实现

--- black-news-service/black-news-service-wemedia/black-news-service-wemedia-server/src/main/java/com/shenzhen/dai/wemedia/service/WmNewsService.java
+++ black-news-service/black-news-service-wemedia/black-news-service-wemedia-server/src/main/java/com/shenzhen/dai/wemedia/service/WmNewsService.java
@@ -24,5 +24,10 @@ public interface WmNewsService extends IService<WmNews> {
      */
     public ResponseResult submitNews(WmNewsDto dto);
 
-
+    /**
+     * 文章的上下架
+     * @param dto
+     * @return
+     */
+    public ResponseResult downOrUp(WmNewsDto dto);
 }
